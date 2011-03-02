2d79f1788d87889d8d1701df8630f1aac6d87831
Nate Murray <nate@natemurray.com>
2011-03-01 21:19:41 -0800

    almost done stochastic hill climbing

--- src/clj/clover/algorithms/stochastic/stochastic-hill-climbing.clj
+++ src/clj/clover/algorithms/stochastic/stochastic-hill-climbing.clj
@@ -16,17 +16,30 @@
   (reduce (fn [sum v] 
             (+ sum (if (= v "1") 1 0))) 0 col))
 
+(defn replace-col-at 
+  [idx new-val col] 
+  ;; todo - assert idx isn't bigger than (- (count col) 1)
+  (let [before (take idx col) 
+        after  (drop (+ idx 1) col)]
+    (doall (concat before (list new-val) after))))
+
+(defn random-neighbor [bitstring]
+  (let [pos (Math/round (random-between 0 (count bitstring)))
+        new-val (if (= (nth bitstring pos) "1") "0" "1")]
+    (replace-col-at pos new-val bitstring)))
+
 (defn do-search [iter max-iter num-bits candidate]
-  (if (< (- max-iter 1) iter))
+  (println " > iteration" iter ", best=" (candidate :cost))
+  (if (< (- max-iter 1) iter)
     candidate
     (let [new-vec (random-neighbor (candidate :vector))
           new-cost (onemax new-vec)
           new-candidate (:vector new-vec :cost new-cost)]
       (if (>= new-cost (candidate :cost))
-        (if (= num-bits cost)
+        (if (= num-bits new-cost)
           new-candidate
           (recur (inc iter) max-iter num-bits new-candidate))
-        (recur (inc iter) max-iter num-bits candidate))))
+        (recur (inc iter) max-iter num-bits candidate)))))
 
 (defn search [max-iter num-bits]
   (let [new-vec (random-bitstring num-bits)
@@ -34,9 +47,8 @@
    (do-search 0 max-iter num-bits
               {:vector new-vec :cost new-cost})))
 
-
 (defn -main [& args]
-  (let [num-bits 64
+  (let [num-bits 5 
         max-iter 1000
         best (search max-iter num-bits)] 
     best))
