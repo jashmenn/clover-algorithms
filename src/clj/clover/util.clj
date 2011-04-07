ae9b81f8b0ba8efb8a72bec00d48f2d2799941af
Nate Murray <nate@natemurray.com>
2011-04-06 17:12:29 -0700

    a bit of refactoring

---
+++ src/clj/clover/util.clj
@@ -0,0 +1,11 @@
+
+(ns clover.util
+  (:require [clojure.contrib.math :as math])
+  (:import [java.lang Math]))
+
+(defn euc-2d [p1 p2]
+  (Math/round 
+   (Math/sqrt 
+    (+ (Math/pow (double (- (first p1) (first p2))) 2)
+       (Math/pow (double (- (last  p1) (last  p2))) 2)))))
+
